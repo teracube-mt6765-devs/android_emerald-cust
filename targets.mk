c0210e8c92651f27a43b15af7238bdcdcbbb822b
Gagan Malvi <malvigagan@gmail.com>
2022-05-19 19:13:55 +0200

    telephony: Add APN configuration from R.SW05.T2E
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- targets.mk
+++ targets.mk
@@ -4,9 +4,15 @@
 
 # For use with LineageOS builds only.
 
+EMERALD_CUST_PATH := emerald-cust
+
 # Set OTA property for emerald target.
 ifeq ($(PRODUCT_DEVICE),emerald)
 PRODUCT_PROPERTY_OVERRIDES += lineageos.updater.uri=https://raw.githubusercontent.com/teracube-mt6765-devs/releases/master/los11-emerald.json
 else
 PRODUCT_PROPERTY_OVERRIDES += lineageos.updater.uri=https://raw.githubusercontent.com/teracube-mt6765-devs/releases/master/los11-2e.json
 endif
+
+# APN configuration
+PRODUCT_COPY_FILES += \
+    $(EMERALD_CUST_PATH)/telephony/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
