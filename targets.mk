b4b2d90a9196da4fa9f3064d3fb3d7f5013be01c
Gagan Malvi <malvigagan@gmail.com>
2022-06-17 08:20:21 +0530

    emerald-cust: targets: add a friendly warning.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- targets.mk
+++ targets.mk
@@ -4,7 +4,11 @@
 
 EMERALD_CUST_PATH := emerald-cust
 
+# Warn users you've included the cust package
+$(warn "Compiling inline with the emerald-cust package.")
+
 ifneq ($(LINEAGE_BUILD),)
+$(warn "Adding OTA overrides for LineageOS")
 # Set OTA property for emerald target.
 ifeq ($(PRODUCT_DEVICE),emerald)
 PRODUCT_PROPERTY_OVERRIDES += lineageos.updater.uri=https://raw.githubusercontent.com/teracube-mt6765-devs/releases/master/los11-emerald.json
