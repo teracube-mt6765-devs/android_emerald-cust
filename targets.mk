a73e760669808eccc32fa037de526edba15204cf
Gagan Malvi <malvigagan@gmail.com>
2022-06-17 08:09:40 +0530

    emerald-cust: Guard with LINEAGE_BUILD
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- targets.mk
+++ targets.mk
@@ -2,15 +2,15 @@
 # Copyright (C) 2022 Teracube-Inc
 #
 
-# For use with LineageOS builds only.
-
 EMERALD_CUST_PATH := emerald-cust
 
+ifneq ($(LINEAGE_BUILD),)
 # Set OTA property for emerald target.
 ifeq ($(PRODUCT_DEVICE),emerald)
 PRODUCT_PROPERTY_OVERRIDES += lineageos.updater.uri=https://raw.githubusercontent.com/teracube-mt6765-devs/releases/master/los11-emerald.json
 else
 PRODUCT_PROPERTY_OVERRIDES += lineageos.updater.uri=https://raw.githubusercontent.com/teracube-mt6765-devs/releases/master/los11-2e.json
+endif
 endif
 
 # APN configuration
